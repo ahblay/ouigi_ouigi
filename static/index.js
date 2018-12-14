ffab8b0a08d9d1ce1b1ef691d58aebb826527ea7
Abel Romer <abel.romer@questu.ca>
2018-12-13 23:14:33 -0800

    Fixed cycle issue with upvotes.

--- static/index.js
+++ static/index.js
@@ -4,17 +4,22 @@ $(".upvote").click(cycleUp);
 
 $(".downvote").click(cycleDown);
 
+function mod(x, n) {
+    result = (x % n + n) % n;
+    return result
+}
+
 function cycleUp() {
     console.log("Upvote selected.")
     let currentLetter = $(".letter").text();
     let index = alphabet.indexOf(currentLetter);
-    let nextLetter = alphabet[(index - 1) % 26];
+    let nextLetter = alphabet[mod((index - 1), 26)];
     $(".letter").text(nextLetter)
 }
 
 function cycleDown() {
     let currentLetter = $(".letter").text();
     let index = alphabet.indexOf(currentLetter);
-    let nextLetter = alphabet[(index + 1) % 26];
+    let nextLetter = alphabet[mod((index + 1), 26)];
     $(".letter").text(nextLetter)
 }
