eb2950267105296c9b96e0d69c7c22a3ff970208
Abel Romer <abel.romer@questu.ca>
2018-12-14 20:48:40 -0800

    Timed updating.

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "shinedownisalittlebad"
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'ouigi'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ouigi_ouigi'
+app.config['MYSQL_DATABASE_DB'] = 'ouigi'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
+
+current_id = 13
+
+
+def add_new_row():
+    sql = "INSERT INTO letters () VALUES ()"
+    cursor.execute(sql)
+    conn.commit()
+    global current_id
+    current_id += 1
 
 
 @app.route("/")
@@ -9,3 +30,43 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/update_letter", methods=["POST"])
+def update_letter():
+    letter = request.form["letter"]
+    sql = "SELECT " + letter + " FROM letters WHERE id = %s"
+    cursor.execute(sql, current_id)
+    value = cursor.fetchone()[0]
+
+    sql = "UPDATE letters SET " + letter + " = %s WHERE id = %s"
+    val = (value + 1, current_id)
+    cursor.execute(sql, val)
+    conn.commit()
+
+    return "success"
+
+
+@app.route("/get_time", methods=["GET"])
+def get_time():
+    now = datetime.now()
+    return jsonify(now.strftime("%S"))
+
+
+@app.route("/get_chosen_letter", methods=["GET"])
+def get_chosen_letter():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    sql = "SELECT a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z FROM letters WHERE id = %s"
+    cursor.execute(sql, current_id)
+    values = cursor.fetchone()
+    print(values)
+    result = "a"
+    max = 0
+    counter = 0
+    for value in values:
+        if value > max:
+            max = value
+            result = alphabet[counter]
+        counter += 1
+
+    add_new_row()
+
+    return jsonify(result)
--- static/index.js
+++ static/index.js
@@ -1,8 +1,18 @@
-var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G",
+                "H", "I", "J", "K", "L", "M", "N",
+                "O", "P", "Q", "R", "S", "T", "U",
+                "V", "W", "X", "Y", "Z"];
+var selectedLetter = "a";
+var timer = null;
+//var interval = setInterval(getChosenLetter, 5000);
+
+$(getTime);
 
 $(".upvote").click(cycleUp);
 
 $(".downvote").click(cycleDown);
+
+$(".submit").click(submit);
 
 function mod(x, n) {
     result = (x % n + n) % n;
@@ -10,16 +20,39 @@ function mod(x, n) {
 }
 
 function cycleUp() {
-    console.log("Upvote selected.")
     let currentLetter = $(".letter").text();
     let index = alphabet.indexOf(currentLetter);
     let nextLetter = alphabet[mod((index - 1), 26)];
-    $(".letter").text(nextLetter)
+    $(".letter").text(nextLetter);
+    selectedLetter = nextLetter.toLowerCase();
 }
 
 function cycleDown() {
     let currentLetter = $(".letter").text();
     let index = alphabet.indexOf(currentLetter);
     let nextLetter = alphabet[mod((index + 1), 26)];
-    $(".letter").text(nextLetter)
+    $(".letter").text(nextLetter);
+    selectedLetter = nextLetter.toLowerCase();
+}
+
+function submit() {
+    $.post("/update_letter", {letter: selectedLetter})
+}
+
+function getTime() {
+    $.get("/get_time", function(time) {
+        console.log(time);
+        let remainingTime = (60 - parseInt(time)) * 1000;
+        setTimeout(function() {
+            setInterval(getChosenLetter, 5000);
+        }, remainingTime);
+    })
+}
+
+function getChosenLetter() {
+    $.get("/get_chosen_letter", function(letter) {
+        console.log(letter)
+        new_text = $(".text").text() + letter
+        $(".text").text(new_text)
+    })
 }
