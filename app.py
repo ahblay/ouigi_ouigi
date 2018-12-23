4ee943510e05ca3d06af08698fbe52234cae04de
Abel Romer <abel.romer@questu.ca>
2018-12-22 23:28:49 -0800

    Debugging.

--- app.py
+++ app.py
@@ -8,29 +8,21 @@ app.secret_key = "shinedownisalittlebad"
 
 db = os.environ['DATABASE_URL']
 conn = psycopg2.connect(db)
-print(conn, 11)
-print(conn.cursor())
 
 current_id = None
 
 
 def get_last_row(cursor):
-    print("GETTING LAST ROW")
     sql = "SELECT MAX(id) FROM letters"
     cursor.execute(sql)
-    print(cursor.fetchone())
     value = cursor.fetchone()[0]
-    print(value, 22)
     if not value:
-        print("NOT VALUE")
         sql = "INSERT INTO letters DEFAULT VALUES"
         cursor.execute(sql)
         conn.commit()
-        print("INSERTED INTO DB")
         sql = "SELECT MAX(id) FROM letters"
         cursor.execute(sql)
         value = cursor.fetchone()[0]
-    print(value, 28)
     return value
 
 
@@ -43,12 +35,11 @@ def add_new_row(cursor):
 
 
 def get_letter_string(cursor):
-    print("GETTING LETTER STRING", 41)
     sql = "SELECT string FROM strings WHERE DATE(datetime) = CURRENT_DATE"
     cursor.execute(sql)
-    value = cursor.fetchone()[0]
-
-    print(value, 46)
+    value = cursor.fetchone()
+    if value:
+        value = value[0]
 
     return value
 
@@ -64,7 +55,7 @@ def add_letter_to_db(letter, cursor):
         value = ""
 
     sql = "UPDATE strings SET string = CONCAT(string, %s) WHERE DATE(datetime) = CURRENT_DATE"
-    cursor.execute(sql, [letter])
+    cursor.execute(sql, (letter,))
     conn.commit()
 
     return value + letter
@@ -84,12 +75,12 @@ def update_letter():
 
     letter = request.form["letter"]
     sql = "SELECT " + letter + " FROM letters WHERE id = %s"
-    cursor.execute(sql, [current_id])
+    cursor.execute(sql, (current_id,))
     value = cursor.fetchone()[0]
 
     sql = "UPDATE letters SET " + letter + " = %s WHERE id = %s"
     val = (value + 1, current_id)
-    cursor.execute(sql, [val])
+    cursor.execute(sql, val)
     conn.commit()
 
     cursor.close()
@@ -105,18 +96,15 @@ def get_time():
 
 @app.route("/get_chosen_letter", methods=["GET"])
 def get_chosen_letter():
-    print("GETTING CHOSEN LETTER")
     cursor = conn.cursor()
 
     global current_id
     current_id = get_last_row(cursor)
-    print(current_id, 108)
 
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     sql = "SELECT a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z FROM letters WHERE id = '%s'"
-    cursor.execute(sql, [current_id])
+    cursor.execute(sql, (current_id,))
     values = cursor.fetchone()
-    print(values)
     result = "."
     max = 0
     counter = 0
@@ -128,7 +116,6 @@ def get_chosen_letter():
 
     add_new_row(cursor)
     letter_string = add_letter_to_db(result, cursor)
-    print(letter_string, 123)
 
     cursor.close()
 
@@ -138,7 +125,6 @@ def get_chosen_letter():
 @app.route("/get_current_string")
 def get_current_string():
     cursor = conn.cursor()
-    print(cursor, 133)
 
     letter_string = get_letter_string(cursor)
 
